08bef1486a0db47c49909982ffdf883091aaf4a0
grgkimari <grgkimari@gmail.com>
2023-09-11 22:04:02 +0300

    Added test route

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express')
 
 const app = express()
-app.listen(3000)
+const PORT = 3000
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.listen(PORT,() => {
+    console.log("App listening on port " + PORT)
+})
